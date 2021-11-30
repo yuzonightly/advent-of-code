a51c02b0f4ef5751386d889b568154440849fa88
yuzonightly <yuzonakai@gmail.com>
2021-11-30 06:49:23 +0000

    update

--- year/2021/2021_1.rs
+++ year/2021/2021_1.rs
@@ -1,5 +1,23 @@
-use std::io;
+use std::env;
+use std::fs;
+// use std::any::type_name
 
 fn main() {
-    println!("hello aoc 2021");
+    // let input = fs::read_to_string("./year/2021/2021_1.input").expect("fs::read_to_string error.");
+    let input = fs::read_to_string("./year/2021/input/2021_1.input").expect("Error reading file.");
+
+    // println!("{}\n", input);
+    // match std::fs::read("./year/2021/2021_1.input") {
+    //     Ok(bytes) => {
+    //         println!("{:?}\n", &bytes[..]);
+    //         // let split: Vec<u32> = bytes.to
+    //     }
+    //     Err(e) => {
+    //         if e.kind() == std::io::ErrorKind::PermissionDenied {
+    //             eprintln!("please run again with appropriate permissions.");
+    //             return;
+    //         }
+    //         panic!("{}", e);
+    //     }
+    // }
 }
