b69e58c052a78c684f68264df71f7a87c5c09a4d
yuzonightly <yuzonakai@gmail.com>
2021-12-01 07:29:52 +0000

    2021 day1

--- year/2021/2021_1.rs
+++ year/2021/2021_1.rs
@@ -1,23 +1,41 @@
-use std::env;
 use std::fs;
-// use std::any::type_name
+use std::io::{self, Write};
+
+fn puzzle_1(numbers: Vec<u32>) {
+    let mut increasing = 0;
+    let mut previous: u32 = numbers[0];
+    for i in 1..numbers.len() {
+        if numbers[i] > previous {
+            increasing += 1;
+        }
+        previous = numbers[i];
+    }
+    writeln!(io::stdout(), "Puzzle 1: {}", increasing);
+}
+
+fn puzzle_2(mut numbers: Vec<u32>) {
+    let mut increasing = 0;
+    let mut previous: u32 = numbers[0..3].iter().sum();
+    numbers.drain(0..1);
+    while numbers.len() >= 3 {
+        let sum: u32 = numbers[0..3].iter().sum();
+        if sum > previous {
+            increasing += 1;
+        }
+        previous = sum;
+        numbers.drain(0..1);
+    }
+    writeln!(io::stdout(), "Puzzle 2: {}", increasing);
+}
 
 fn main() {
-    // let input = fs::read_to_string("./year/2021/2021_1.input").expect("fs::read_to_string error.");
-    let input = fs::read_to_string("./year/2021/input/2021_1.input").expect("Error reading file.");
+    let input =
+        fs::read_to_string("./year/2021/inputs/day1_puzzle1.input").expect("Error reading file.");
+    let numbers: Vec<u32> = input.lines().map(|s| s.parse::<u32>().unwrap()).collect();
+    puzzle_1(numbers);
 
-    // println!("{}\n", input);
-    // match std::fs::read("./year/2021/2021_1.input") {
-    //     Ok(bytes) => {
-    //         println!("{:?}\n", &bytes[..]);
-    //         // let split: Vec<u32> = bytes.to
-    //     }
-    //     Err(e) => {
-    //         if e.kind() == std::io::ErrorKind::PermissionDenied {
-    //             eprintln!("please run again with appropriate permissions.");
-    //             return;
-    //         }
-    //         panic!("{}", e);
-    //     }
-    // }
+    let input =
+        fs::read_to_string("./year/2021/inputs/day1_puzzle2.input").expect("Error reading file.");
+    let mut numbers: Vec<u32> = input.lines().map(|s| s.parse::<u32>().unwrap()).collect();
+    puzzle_2(numbers);
 }
