80b98938ada98fd4460351e5e601c2c311bffece
yuzonightly <yuzonakai@gmail.com>
2021-12-03 01:44:18 +0000

    2021 day2

--- README.md
+++ README.md
@@ -11,12 +11,12 @@ _Tested on Linux. Does not run on Windows._
 ```bash
 cargo run -- --help
 cargo run -- file FILENAME # runs the specified file
-cargo run -- year YEAR # runs all problems this for the specified year
+cargo run -- year YEAR # runs all puzzles for the specified year
 ```
 
 ### Example
 
 ```bash
-cargo run -- file 2021_1 # runs 2021_1.rs
-cargo run -- year 2021 # runs all problems for 2021
+cargo run -- file 2021_1 # runs 2021_1.rs: year 2021 - day 1
+cargo run -- year 2021 # runs all puzzles for 2021
 ```
--- year/2021/2021_2.rs
+++ year/2021/2021_2.rs
@@ -1,18 +1,117 @@
+use std::error::Error;
 use std::fs;
 use std::io::{self, Write};
+use std::str::{self, FromStr};
 
-fn puzzle_1() {}
+#[derive(Clone, Debug)]
+struct Submarine {
+    position: Position,
+    aim: i32,
+}
 
-fn puzzle_2() {}
+#[derive(Clone, Debug)]
+struct Position {
+    horizontal: i32,
+    depth: i32,
+}
+
+#[derive(Clone, Debug)]
+enum Action {
+    Forward { value: i32 },
+    Up { value: i32 },
+    Down { value: i32 },
+}
+
+impl FromStr for Action {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split: Vec<&str> = s.trim().split_whitespace().collect();
+        let act: String = split[0].to_owned();
+        let value: i32 = split[1].parse()?;
+        let action: Action = match act.as_str() {
+            "forward" => Action::Forward { value },
+            "up" => Action::Up { value },
+            "down" => Action::Down { value },
+            _ => {
+                panic!("FromStr Action error.");
+            }
+        };
+        Ok(action)
+    }
+}
+
+impl Submarine {
+    fn perform_action(&mut self, action: &Action) {
+        match action {
+            Action::Forward { value } => {
+                self.position.horizontal += value;
+            }
+            Action::Up { value } => {
+                self.position.depth -= value;
+            }
+            Action::Down { value } => {
+                self.position.depth += value;
+            }
+        }
+    }
+
+    fn perform_correct_action(&mut self, action: &Action) {
+        match action {
+            Action::Forward { value } => {
+                self.position.horizontal += value;
+                self.position.depth += value * self.aim;
+            }
+            Action::Up { value } => {
+                self.aim -= value;
+            }
+            Action::Down { value } => {
+                self.aim += value;
+            }
+        }
+    }
+}
+
+fn puzzle_1(submarine: &mut Submarine, actions: &Vec<Action>) {
+    for act in actions {
+        submarine.perform_action(&act);
+    }
+    writeln!(
+        io::stdout(),
+        "Puzzle 1: {} * {} = {}",
+        submarine.position.horizontal,
+        submarine.position.depth,
+        submarine.position.depth * submarine.position.horizontal
+    );
+}
+
+fn puzzle_2(submarine: &mut Submarine, actions: &Vec<Action>) {
+    for act in actions {
+        submarine.perform_correct_action(&act);
+    }
+    writeln!(
+        io::stdout(),
+        "Puzzle 2: {} * {} = {}",
+        submarine.position.horizontal,
+        submarine.position.depth,
+        submarine.position.depth * submarine.position.horizontal
+    );
+}
 
 fn main() {
-    let input =
-        fs::read_to_string("./year/2021/inputs/day1_puzzle1.input").expect("Error reading file.");
-    let numbers: Vec<u32> = input.lines().map(|s| s.parse::<u32>().unwrap()).collect();
-    puzzle_1(numbers);
-
-    let input =
-        fs::read_to_string("./year/2021/inputs/day1_puzzle2.input").expect("Error reading file.");
-    let mut numbers: Vec<u32> = input.lines().map(|s| s.parse::<u32>().unwrap()).collect();
-    puzzle_2(numbers);
+    let input = fs::read_to_string("./year/2021/inputs/day2.input").expect("Error reading file.");
+    let actions: Vec<Action> = input
+        .lines()
+        .map(|a| a.parse::<Action>().unwrap())
+        .collect();
+    let mut submarine = Submarine {
+        position: Position {
+            horizontal: 0,
+            depth: 0,
+        },
+        aim: 0,
+    };
+    let mut new_submarine = submarine.clone();
+    puzzle_1(&mut submarine, &actions);
+    puzzle_2(&mut new_submarine, &actions);
 }
