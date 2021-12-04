ff024c9b3656aac5d99b634f58338f6e04672bc9
yuzonightly <yuzonakai@gmail.com>
2021-12-04 07:36:24 +0000

    2021 day4

--- year/2020/2020_4.rs
+++ year/2020/2020_4.rs
@@ -1,28 +1,78 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 use std::io::{self, Write};
 use std::str::{self, FromStr};
 
-// #[derive(Clone, Debug)]
-// struct Action {
-//     act: i32,
-// }
+#[derive(Clone, Debug)]
+struct Passports(Vec<Passport>);
 
-// impl FromStr for Action {
-//     type Err = Box<dyn Error>;
-
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {}
-// }
-
-fn puzzle_1() {
-    writeln!(io::stdout(), "Puzzle 1: {}", 1);
+#[derive(Clone, Debug)]
+struct Passport {
+    info: HashMap<String, String>,
 }
 
-fn puzzle_2() {
-    writeln!(io::stdout(), "Puzzle 2: {}", 1);
+impl FromStr for Passports {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let passports: Vec<&str> = s.split("\n\n").collect();
+        let mut vec: Vec<Passport> = Vec::new();
+        for pass in &passports {
+            let pass: Vec<&str> = pass.split_whitespace().collect();
+            let mut map: HashMap<String, String> = HashMap::new();
+            for data in pass {
+                let split: Vec<&str> = data.split(":").collect();
+                map.insert(split[0].to_owned(), split[1].to_owned());
+            }
+            vec.push(Passport { info: map });
+        }
+        Ok(Passports(vec))
+    }
+}
+
+fn puzzle_1(passports: &Vec<Passport>) {
+    let to_find = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    let mut valid_passports = 0;
+    for passport in passports {
+        let mut correctness = 0;
+        for &data in &to_find {
+            match passport.info.get(data) {
+                Some(key) => {
+                    valid_passports -= 1;
+                    break;
+                }
+                _ => {}
+            }
+        }
+        if correctness == 7 {
+            valid_passports += 1;
+        }
+    }
+    writeln!(io::stdout(), "Puzzle 1: {}", valid_passports);
+}
+
+fn puzzle_2(passports: &Vec<Passport>) {
+    let to_find = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    let mut valid_passports = passports.len();
+    for passport in passports {
+        for &data in &to_find {
+            match passport.info.get(data) {
+                Some(value) => {
+                    // valid_passports -= 1;
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+    writeln!(io::stdout(), "Puzzle 2: {}", valid_passports);
 }
 
 fn main() {
     let input = fs::read_to_string("./year/2020/inputs/day4.input").expect("Error reading file.");
-    puzzle_1();
+    let passports: Passports = input.parse().unwrap();
+    puzzle_1(&passports.0);
+    puzzle_2(&passports.0);
+
 }
--- year/2021/2021_4.rs
+++ year/2021/2021_4.rs
@@ -3,26 +3,142 @@ use std::fs;
 use std::io::{self, Write};
 use std::str::{self, FromStr};
 
-// #[derive(Clone, Debug)]
-// struct Action {
-//     act: i32,
-// }
-
-// impl FromStr for Action {
-//     type Err = Box<dyn Error>;
-
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {}
-// }
-
-fn puzzle_1() {
-    writeln!(io::stdout(), "Puzzle 1: {}", 1);
+#[derive(Clone, Debug)]
+struct Game {
+    calls: Vec<i32>,
+    boards: Vec<Board>,
 }
 
-fn puzzle_2() {
-    writeln!(io::stdout(), "Puzzle 2: {}", 1);
+#[derive(Clone, Debug)]
+struct Board(Vec<Vec<i32>>);
+
+impl FromStr for Game {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lines: Vec<&str> = s.split("\n\n").collect();
+        let calls: Vec<i32> = lines[0]
+            .trim()
+            .split(",")
+            .map(|s| s.parse().unwrap())
+            .collect();
+        let mut boards: Vec<Board> = Vec::new();
+        for board in &lines[1..] {
+            let board_rows: Vec<&str> = board.lines().collect();
+            let mut bingo_board: Vec<Vec<i32>> = Vec::new();
+            for row in board_rows {
+                bingo_board.push(
+                    row.trim()
+                        .split_whitespace()
+                        .map(|s| s.parse::<i32>().unwrap())
+                        .collect::<Vec<i32>>(),
+                );
+            }
+            boards.push(Board(bingo_board));
+        }
+        Ok(Game { calls, boards })
+    }
+}
+
+fn check_board(board: &Vec<Vec<i32>>) -> bool {
+    let mut row = true;
+    let mut col = true;
+    for i in 0..board.len() {
+        row = true;
+        col = true;
+        for j in 0..board[i].len() {
+            if board[i][j] != -1 {
+                row = false;
+            }
+            if board[j][i] != -1 {
+                col = false;
+            }
+        }
+        if row || col {
+            return true;
+        }
+    }
+    false
+}
+
+fn puzzle_1(bingo: &mut Game) {
+    let cloned_board = bingo.boards.clone();
+    let calls = &bingo.calls;
+    'outer: for call in calls {
+        for board_index in 0..bingo.boards.len() {
+            let mut board = &mut bingo.boards[board_index];
+            for i in 0..board.0.len() {
+                for j in 0..board.0[i].len() {
+                    if board.0[i][j] == *call {
+                        board.0[i][j] = -1;
+                        if check_board(&board.0) {
+                            let sum = {
+                                let clone = &cloned_board[board_index];
+                                let mut sum = 0;
+                                for _i in 0..clone.0.len() {
+                                    for _j in 0..clone.0[_i].len() {
+                                        if board.0[_i][_j] != -1 {
+                                            sum += clone.0[_i][_j];
+                                        }
+                                    }
+                                }
+                                sum
+                            };
+                            writeln!(io::stdout(), "Puzzle 1: {}", call * sum);
+                            break 'outer;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn puzzle_2(bingo: &mut Game) {
+    let cloned_board = bingo.boards.clone();
+    let calls = &bingo.calls;
+    let last_board = bingo.boards.len();
+    let mut board_completion = 0;
+    'outer: for call in calls {
+        for board_index in 0..bingo.boards.len() {
+            let mut board = &mut bingo.boards[board_index];
+            for i in 0..board.0.len() {
+                for j in 0..board.0[i].len() {
+                    if check_board(&board.0) {
+                        break;
+                    }
+                    if board.0[i][j] == *call {
+                        board.0[i][j] = -1;
+                        if check_board(&board.0) {
+                            board_completion += 1;
+                            if board_completion == last_board {
+                                let sum = {
+                                    let clone = &cloned_board[board_index];
+                                    let mut sum = 0;
+                                    for _i in 0..clone.0.len() {
+                                        for _j in 0..clone.0[_i].len() {
+                                            if board.0[_i][_j] != -1 {
+                                                sum += clone.0[_i][_j];
+                                            }
+                                        }
+                                    }
+                                    sum
+                                };
+                                writeln!(io::stdout(), "Puzzle 2: {}", call * sum);
+                                break 'outer;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 fn main() {
     let input = fs::read_to_string("./year/2021/inputs/day4.input").expect("Error reading file.");
-    puzzle_1();
+    let mut game: Game = input.parse().unwrap();
+    let mut new_game = game.clone();
+    puzzle_1(&mut game);
+    puzzle_2(&mut new_game);
 }
