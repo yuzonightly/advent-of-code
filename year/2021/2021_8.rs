82a7fb9fff311d7ec080c6c6756246c56f52aa7a
yuzonightly <yuzonakai@gmail.com>
2021-12-12 04:14:23 +0000

    2021 day8

--- year/2021/2021_8.rs
+++ year/2021/2021_8.rs
@@ -9,7 +9,7 @@ fn puzzle_1(input: &Vec<&str>) {
     let unique: [usize; 4] = [2, 3, 4, 7];
     for instance in input {
         let inp_out: Vec<&str> = instance.split("|").collect::<Vec<&str>>();
-        let mut out: Vec<HashSet<char>> = inp_out[1]
+        let out: Vec<HashSet<char>> = inp_out[1]
             .trim()
             .split(" ")
             .map(|s| s.chars().collect::<HashSet<char>>())
@@ -24,7 +24,7 @@ fn puzzle_1(input: &Vec<&str>) {
 }
 
 fn puzzle_2(input: &Vec<&str>) {
-    let mut count = 0;
+    let mut sum = 0;
     for instance in input {
         let inp_out: Vec<&str> = instance.split("|").collect::<Vec<&str>>();
         let mut inp: Vec<Vec<char>> = inp_out[0]
@@ -32,55 +32,96 @@ fn puzzle_2(input: &Vec<&str>) {
             .split(" ")
             .map(|s| s.chars().collect::<Vec<char>>())
             .collect();
-        let mut out: Vec<Vec<char>> = inp_out[1]
+        let out: Vec<Vec<char>> = inp_out[1]
             .trim()
             .split(" ")
             .map(|s| s.chars().collect::<Vec<char>>())
             .collect();
         inp.sort_by(|a, b| a.len().cmp(&b.len()));
-        let mut segment_map = HashMap::new();
-        for segments in &inp {
-            let mut set: HashSet<i32>;
-            if segments.len() == 2 {
-                set = vec![2, 5].iter().cloned().collect();
-                segment_map.insert(segments[0], set.clone());
-                segment_map.insert(segments[1], set.clone());
-            } else if segments.len() == 3 {
-                set = vec![0].iter().cloned().collect();
-                for c in segments {
-                    if !segment_map.contains_key(c) {
-                        segment_map.insert(*c, set.clone());
-                    }
-                }
-            } else if segments.len() == 4 {
-                set = vec![1, 3].iter().cloned().collect();
-                for c in segments {
-                    if !segment_map.contains_key(c) {
-                        segment_map.insert(*c, set.clone());
-                    }
-                }
-            } else if segments.len() == 7 {
-                set = vec![4, 6].iter().cloned().collect();
-                for c in segments {
-                    if !segment_map.contains_key(c) {
-                        segment_map.insert(*c, set.clone());
-                    }
-                }
+        let mut segment_connections: HashMap<char, Vec<usize>> = HashMap::new();
+        let mut segments = &inp[0];
+        segment_connections.insert(segments[0], vec![2, 5]);
+        segment_connections.insert(segments[1], vec![2, 5]);
+        segments = &inp[1];
+        for seg in segments {
+            if !segment_connections.contains_key(&seg) {
+                segment_connections.insert(*seg, vec![0]);
             }
         }
-        let numbers: Vec<Vec<char>> = Vec::new();
-        for segments in &inp {
-            for (key, value) in segment_map {
-                if segments.len() == 6 && !segments.contains(key) {
-                    
-                }
-
-            } 
-                
-            )
+        segments = &inp[2];
+        for seg in segments {
+            if !segment_connections.contains_key(&seg) {
+                segment_connections.insert(*seg, vec![1, 3]);
+            }
         }
+        segments = &inp[9];
+        for seg in segments {
+            if !segment_connections.contains_key(&seg) {
+                segment_connections.insert(*seg, vec![4, 6]);
+            }
+        }
+        let segment_letters = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+        for segments in &inp {
+            if segments.len() == 6 {
+                let mut missing_segment = 's';
+                for letter in &segment_letters {
+                    if !segments.contains(&letter) {
+                        missing_segment = *letter;
+                        break;
+                    }
+                }
+                let mut segment_connections_clone = segment_connections.clone();
+                let values = segment_connections.get(&missing_segment).unwrap();
+                for (key, value) in &segment_connections {
+                    if *values == *value && *key == missing_segment {
+                        let s = segment_connections_clone.get_mut(&key).unwrap();
+                        if value.contains(&2) {
+                            *s = vec![2];
+                        } else if value.contains(&1) {
+                            *s = vec![3];
+                        } else if value.contains(&4) {
+                            *s = vec![4];
+                        }
+                    } else if *values == *value && *key != missing_segment {
+                        let s = segment_connections_clone.get_mut(&key).unwrap();
+                        if value.contains(&2) {
+                            *s = vec![5];
+                        } else if value.contains(&1) {
+                            *s = vec![1];
+                        } else if value.contains(&4) {
+                            *s = vec![6];
+                        }
+                    }
+                }
+                segment_connections = segment_connections_clone;
+            }
+        }
+        let numbers: Vec<Vec<usize>> = vec![
+            vec![0, 1, 2, 4, 5, 6],
+            vec![2, 5],
+            vec![0, 2, 3, 4, 6],
+            vec![0, 2, 3, 5, 6],
+            vec![1, 2, 3, 5],
+            vec![0, 1, 3, 5, 6],
+            vec![0, 1, 3, 4, 5, 6],
+            vec![0, 2, 5],
+            vec![0, 1, 2, 3, 4, 5, 6],
+            vec![0, 1, 2, 3, 5, 6],
+        ];
+        let mut sum_instance = 0;
+        for segment in &out {
+            let mut coded_number = Vec::new();
+            for letter in segment {
+                let mut temp = segment_connections.get(&letter).unwrap().clone();
+                coded_number.append(&mut temp);
+            }
+            coded_number.sort();
+            let decoded_number = numbers.iter().position(|r| *r == coded_number).unwrap();
+            sum_instance = sum_instance * 10 + decoded_number;
+        }
+        sum += sum_instance;
     }
-    // writeln!(io::stdout(), "Puzzle 2: {:?}", segment_map);
+    writeln!(io::stdout(), "Puzzle 2: {:?}", sum);
 }
 
 fn main() {
